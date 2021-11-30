85d1ea1e65c42a16a767eb0ef17f80305f22a5c9
Jonas S. Bezerra <jonassantosbezerra@outlook.com>
2021-11-30 11:01:55 -0300

    feat: add function  to Response module

--- lib/mastery/core/response.ex
+++ lib/mastery/core/response.ex
@@ -3,5 +3,28 @@ defmodule Mastery.Core.Response do
     The response to a `Mastery.Core.Question` given by the user.
   """
 
+  alias Mastery.Core.Quiz
+
   defstruct ~w[quiz_title template_name to email answer correct timestamp]a
+
+  @doc """
+  Creates a new response for the given `quiz`, `email` and `answer`.
+
+  Returns `Response`.
+  """
+  @spec new(Quiz.t(), String.t(), String.t()) :: Response.t()
+  def new(quiz, email, answer) do
+    question = quiz.current_question
+    template = question.template
+
+    %__MODULE__{
+      quiz_title: quiz.title,
+      template_name: template.name,
+      to: question.asked,
+      email: email,
+      answer: answer,
+      correct: template.checker.(question.substitutions, answer),
+      timestamp: DateTime.utc_now()
+    }
+  end
 end
