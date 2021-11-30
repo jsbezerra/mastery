402d386abceba9846ae5f797b4f642780ad48004
Jonas S. Bezerra <jonassantosbezerra@outlook.com>
2021-11-30 17:36:25 -0300

    feat: instantiating templates and questions

--- lib/mastery/core/template.ex
+++ lib/mastery/core/template.ex
@@ -3,4 +3,13 @@ defmodule Mastery.Core.Template do
     Templates are used to group, generate and check the responses of questions on a quiz.
   """
   defstruct ~w[name category instructions raw compiled generators check]a
+
+  def new(fields) do
+    raw = Keyword.fetch!(fields, :raw)
+
+    struct!(
+      __MODULE__,
+      Keyword.put(fields, :compiled, EEx.compile_string(raw))
+    )
+  end
 end
