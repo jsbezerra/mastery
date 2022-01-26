3da2c5354496da69af61b47fadc0f5ca9c3b5ca5
Jonas S. Bezerra <jonassantosbezerra@outlook.com>
2022-01-26 19:28:31 -0300

    test: add helper to create template fields

--- test/support/quiz_builders.exs
+++ test/support/quiz_builders.exs
@@ -7,4 +7,18 @@ defmodule QuizBuilders do
   end
 
   alias Mastery.Core.{Quiz, Response, Template}
+
+  def template_fields(overrides \\ []) do
+    Keyword.merge(
+      [
+        name: :single_digit_addition,
+        category: :addition,
+        instructions: "Add the numbers",
+        raw: "<%= @left %> + <%= @right %>",
+        generators: addition_generators(single_digits()),
+        checker: &addition_checker/2
+      ],
+      overrides
+    )
+  end
 end
