1085dc90a7b872554bf6b638ec5b22cb660d2313
Jonas S. Bezerra <jonassantosbezerra@outlook.com>
2022-01-26 22:28:58 -0300

    fix: test file renaming and wrong property name in Template

--- lib/mastery/core/template.ex
+++ lib/mastery/core/template.ex
@@ -2,7 +2,7 @@ defmodule Mastery.Core.Template do
   @moduledoc """
     Templates are used to group, generate and check the responses of questions on a quiz.
   """
-  defstruct ~w[name category instructions raw compiled generators check]a
+  defstruct ~w[name category instructions raw compiled generators checker]a
 
   def new(fields) do
     raw = Keyword.fetch!(fields, :raw)
